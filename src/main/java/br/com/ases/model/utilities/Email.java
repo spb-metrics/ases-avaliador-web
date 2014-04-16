1d33ea4a1ee124bac48df18204a6292aa05f1324
lyandro.santana <lyandro.santana@c2178572-b5ca-4887-91d2-9e3a90c7d55b>
2014-04-16 06:23:07 +0000

    Tarefa - 417. Solicitar Contato.
    git-svn-id: https://svn.bento.ifrs.edu.br/default/ASES/ASES%20-%20Web/ASES%20-%20Web/Fontes/avaliador-web@9261 c2178572-b5ca-4887-91d2-9e3a90c7d55b

--- src/main/java/br/com/ases/model/utilities/Email.java
+++ src/main/java/br/com/ases/model/utilities/Email.java
@@ -22,8 +22,8 @@ public class Email {
 	
 	private final String HOST = "smtp.gmail.com";
 	private final int PORT = 465;
-	private final String AUTHUSER = "suportetiif";
-	private final String AUTHPASS = "@suporteif";
+	private final String AUTHUSER = "";
+	private final String AUTHPASS = "";
     
 	/**
 	 * @param String host, int port, String authUser, String authPass*/
