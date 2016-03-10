e9b7a71beb01741b78cda8e2fd95707af64c7f64
higo.gomes <higo.gomes@c2178572-b5ca-4887-91d2-9e3a90c7d55b>
2016-03-10 20:52:36 +0000

    git-svn-id: https://svn.bento.ifrs.edu.br/default/ASES/ASES%20-%20Web/ASES%20-%20Web/Fontes/avaliador-web@10603 c2178572-b5ca-4887-91d2-9e3a90c7d55b

--- src/main/webapp/inicial_arquivos/js/acessibilidade.js
+++ src/main/webapp/inicial_arquivos/js/acessibilidade.js
@@ -1,7 +1,57 @@
 
-/**
-* FUNCAO QUE LIMPA OS INPUT TEXT.
-*/
-function clearDefault(el) {
-  if (el.defaultValue==el.value) el.value = ""
+
+//Contraste
+function configContrastePadrao(){
+	if(!$.cookie("contraste"))
+	{
+		$.cookie("contraste", "", { path: '/' });
+	}
+
+	if($.cookie("contraste") && $.cookie("contraste") != "")
+	{
+		$("body").addClass($.cookie("contraste"));
+	}
 }
+
+function selecionaContraste(){   
+	
+	if ($.cookie("contraste") == "")
+	{	
+		$("body").addClass("contraste");
+		
+		$.cookie("contraste", "contraste", { path: '/' });
+	}
+	else
+	{
+		$('body').removeClass("contraste")
+		
+		$.cookie("contraste", "", { path: '/' });
+	}
+}
+
+$(document).ready(function() {
+	configContrastePadrao();
+
+	$("#ancoraConteudo").click(function(){ gotoConteudo(); return false;});
+	$("#ancoraMenu").click(function(){ gotoMenu(); return false;});
+	$("#altoContraste").click(function(){selecionaContraste(); return false;});
+	
+	$(document).bind("keydown", "Alt+Shift+1", function(){ gotoConteudo(); return false;});
+	$(document).bind("keydown", "Alt+1", function(){ gotoConteudo(); return false;});	
+		
+	$(document).bind("keydown", "Alt+Shift+2", function(){ gotoMenu(); return false;});
+	$(document).bind("keydown", "Alt+2", function(){ gotoMenu(); return false;});
+
+	$(document).bind("keydown", "Alt+Shift+4", function(){ selecionaContraste(); return false;});
+	$(document).bind("keydown", "Alt+4", function(){ selecionaContraste(); return false;});
+
+	$(document).bind("keydown", "Alt+Shift+5", function(){ gotoAcessibilidade(); return false;});
+	$(document).bind("keydown", "Alt+5", function(){ gotoAcessibilidade(); return false;});
+
+	$(document).bind("keydown", "Alt+Shift+6", function(){ gotoAnterior(); return false;});
+	$(document).bind("keydown", "Alt+6", function(){ gotoAnterior(); return false;});
+
+	$(document).bind("keydown", "Alt+Shift+7", function(){ gotoSeguinte(); return false;});
+	$(document).bind("keydown", "Alt+7", function(){ gotoSeguinte(); return false;});
+});
+
