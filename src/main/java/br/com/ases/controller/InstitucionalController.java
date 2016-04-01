572e650f53d8819999e870ae0c2738c1150a7c17
higo.gomes <higo.gomes@c2178572-b5ca-4887-91d2-9e3a90c7d55b>
2016-04-01 13:42:26 +0000

    git-svn-id: https://svn.bento.ifrs.edu.br/default/ASES/ASES%20-%20Web/ASES%20-%20Web/Fontes/avaliador-web@10659 c2178572-b5ca-4887-91d2-9e3a90c7d55b

--- src/main/java/br/com/ases/controller/InstitucionalController.java
+++ src/main/java/br/com/ases/controller/InstitucionalController.java
@@ -1,19 +1,31 @@
 package br.com.ases.controller;
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.client.methods.HttpGet;
+import org.jfree.chart.title.Title;
+import org.omg.CORBA.PRIVATE_MEMBER;
+import org.w3c.dom.html.HTMLTitleElement;
+
+import com.jcabi.http.Request;
 
 import br.com.ases.model.utilities.ManagerXml;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.Validator;
+import br.com.caelum.vraptor.ioc.spring.VRaptorRequestHolder;
 
 
 @Resource
 public class InstitucionalController {
 	
+	private String tituloPagina;
 	private Result result;
 	private final Validator validator;
 	private ServletContext application; 
+	
 	
 	public InstitucionalController(Result result, Validator validator, ServletContext application) {
 		this.result = result;
@@ -23,6 +35,10 @@ public class InstitucionalController {
 	
 	@Path("/perguntas-frequentes")
 	public void perguntasFrequentes() {
+		
+		tituloPagina = "Perguntas frequentes - ASES";
+		//Seta o título da página 
+		VRaptorRequestHolder.currentRequest().getServletContext().setAttribute("tituloPagina", tituloPagina);
 		
 		ManagerXml managerXml = new ManagerXml(this.application.getRealPath("")+"/WEB-INF/perguntas-frequentes.xml");
 		
@@ -36,9 +52,24 @@ public class InstitucionalController {
 	}
 	
 	@Path("/acessibilidade")
-	public void acessibilidade() {}
+	public void acessibilidade() {
+		tituloPagina = "Acessibilidade - ASES";
+		//Seta o título da página 
+		VRaptorRequestHolder.currentRequest().getServletContext().setAttribute("tituloPagina", tituloPagina);
+	}
 	
 	@Path("/mapa-site")
-	public void mapa() {}
+	public void mapa() {
+		tituloPagina = "Mapa do site - ASES";
+		//Seta o título da página 
+		VRaptorRequestHolder.currentRequest().getServletContext().setAttribute("tituloPagina", tituloPagina);
+	}
+	
+	@Path("/criteriosSucesso")
+	public void criteriosSucesso() {
+		tituloPagina = "Critérios de sucesso - ASES";
+		//Seta o título da página 
+		VRaptorRequestHolder.currentRequest().getServletContext().setAttribute("tituloPagina", tituloPagina);
+	}
 	
 }
