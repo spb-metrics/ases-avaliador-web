9a8ebc03fc2d8550ef833a01c326d9ab340a3ca1
higo.gomes <higo.gomes@c2178572-b5ca-4887-91d2-9e3a90c7d55b>
2016-04-01 13:41:55 +0000

    git-svn-id: https://svn.bento.ifrs.edu.br/default/ASES/ASES%20-%20Web/ASES%20-%20Web/Fontes/avaliador-web@10658 c2178572-b5ca-4887-91d2-9e3a90c7d55b

--- src/main/java/br/com/ases/controller/IndexController.java
+++ src/main/java/br/com/ases/controller/IndexController.java
@@ -3,9 +3,11 @@ package br.com.ases.controller;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.ioc.spring.VRaptorRequestHolder;
 @Resource
 public class IndexController {
 	
+	private String tituloPagina = "ASES - Avaliador e Simulador de AcessibilidadE em Sites";
 	private Result result;
 	
 	public IndexController (Result result) {
@@ -22,6 +24,8 @@ public class IndexController {
 	public void about() {}
 	
 	@Path("/")
-	public void index() {}
-
+	public void index() {
+		VRaptorRequestHolder.currentRequest().getServletContext().setAttribute("tituloPagina", tituloPagina);
+	}
+	
 }
