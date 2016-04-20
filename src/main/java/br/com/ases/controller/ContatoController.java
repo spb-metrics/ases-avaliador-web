3db4f540f8959b419687182826a532e71eb76422
gibran <gibransodre@gmail.com>
2016-04-20 15:46:41 -0300

    Versão corrigida alguns bug's de avaliação

--- src/main/java/br/com/ases/controller/ContatoController.java
+++ src/main/java/br/com/ases/controller/ContatoController.java
@@ -30,56 +30,58 @@ public class ContatoController {
 	private Result result;
 	private Validator validator;
 	private ServletContext application;
-	
-	
-	
-	public ContatoController(Result result, Validator validator,ServletContext application) {
-		
+
+	public ContatoController(Result result, Validator validator,
+			ServletContext application) {
+
 		this.result = result;
 		this.validator = validator;
 		this.application = application;
-		
-		//Seta o título da página 
-		VRaptorRequestHolder.currentRequest().getServletContext().setAttribute("tituloPagina", tituloPagina);
+
+		// Seta o título da página
+		VRaptorRequestHolder.currentRequest().getServletContext()
+				.setAttribute("tituloPagina", tituloPagina);
 	}
-	
+
 	@Path("")
 	public void index() {
 
 	}
-	
+
 	@Post("/confirmacao")
-	public void confirmation(Contato contato) throws EmailException, MalformedURLException, UnsupportedEncodingException{
-		
+	public void confirmation(Contato contato) throws EmailException,
+			MalformedURLException, UnsupportedEncodingException {
+
 		Validate validate = new Validate(this.validator);
-		
-		if(validate.contato(contato)){
-			
-			Email email = new Email(this.application.getRealPath("")+"/WEB-INF/mail.properties");
-			String respSendEmail = email.sendEmail("Contato", 
-					"govbr@planejamento.gov.br", 
-					contato.getNome(), 
-					contato.getEmail(), 
+
+		if (validate.contato(contato)) {
+
+			Email email = new Email(this.application.getRealPath("")
+					+ "/WEB-INF/mail.properties");
+			String respSendEmail = email.sendEmail(
+					"Contato",
+					"govbr@planejamento.gov.br", contato.getNome(),
+					contato.getEmail(),
 					StringHelper.convertFromUTF8(contato.getAssunto()),
-					StringHelper.convertFromUTF8(contato.getMensagem()),
-					true
-					);
-			
-			if(respSendEmail.equals("")){
+					StringHelper.convertFromUTF8(contato.getMensagem()), true);
+
+			if (respSendEmail.equals("")) {
 				result.include("mensagem", "Mensagem enviada com Sucesso");
 				result.include("status", "success");
-				
-			}else{
-				result.include("mensagem", "N&atilde; foi poss&iacute;vel enviar a mensagem! Ocorreu o problema: "+respSendEmail);
-				result.include("status", "error");				
+
+			} else {
+				result.include("mensagem",
+						"Não foi possível enviar a mensagem! Ocorreu o problema: " + respSendEmail);
+				result.include("status", "error");
 			}
-			
+
 			this.result.redirectTo(ContatoController.class).index();
-			
-		}else{
-			 this.validator = validate.getMessage();
-			 this.validator.onErrorUsePageOf(ContatoController.class).index();
+
+		} else {
+			this.validator = validate.getMessage();
+			this.validator.onErrorUsePageOf(ContatoController.class).index();
+
 		}
 	}
-	
+
 }
--- src/main/java/br/com/ases/infra/EseloProperties.java
+++ src/main/java/br/com/ases/infra/EseloProperties.java
@@ -15,6 +15,9 @@ public class EseloProperties {
 	private static String PROPERTIES_PATH = "/WEB-INF/eselo.properties";
 	private Properties properties = new Properties();
 	
+	public EseloProperties() {
+	}
+	
 	public EseloProperties(ServletContext servletContext) {
 		
 		try {
@@ -26,5 +29,13 @@ public class EseloProperties {
 			
 	public String getUrl(String key) {
 		return this.properties.getProperty(key);
+	}
+	
+	public String getSem_nota(String sem_nota) {
+		return this.properties.getProperty(sem_nota);
+	}
+	
+	public String getMensagem_avaliacao(String mensagem_avaliacao) {
+		return this.properties.getProperty(mensagem_avaliacao);
 	}
 }
