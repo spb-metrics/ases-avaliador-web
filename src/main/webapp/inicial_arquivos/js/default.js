3615bf655f36e882a492bed1e37a62d73c36886b
gibran <gibransodre@gmail.com>
2016-04-27 12:15:15 -0300

    serve para mudar o foco para mensagem de erro de campo vazio bem como para o campo "vazio" após clicar no link da mensagem

---
+++ src/main/webapp/inicial_arquivos/js/default.js
@@ -0,0 +1,49 @@
+$(document).ready(function()
+
+{
+
+	// funcionalidade para exibir a 'tab' correspondente ao link da mensagem de
+	// erro do campo vazio
+	// e focar no campo obrigatório da página 'index.jsp'
+
+	var focarMensagemErro = true;
+
+	$("#mensagemErroCampoVazio_url").click(function() {
+		focarMensagemErro = false;
+		$("#tab-1").prop("checked", true);
+		$("#url").focus();
+		
+
+	});
+
+	$("#mensagemErroCampoVazio_up_file").click(function() {
+		focarMensagemErro = false;
+		$("#tab-2").prop("checked", true);
+		$("#up_file").focus();
+		
+	});
+
+	$("#mensagemErroCampoVazio_input").click(function() {
+		focarMensagemErro = false;
+		$("#tab-3").prop("checked", true);
+		$("#input").focus();
+		
+	});
+if(focarMensagemErro)
+	{
+	if ($("#mensagemErroCampoVazio_url").length) {
+		$("#mensagemErroCampoVazio_url").focus();			
+	}
+
+	if ($("#mensagemErroCampoVazio_up_file").length) {
+		$("#mensagemErroCampoVazio_up_file").focus();
+		focarMensagemErro = false;
+	}
+
+	if ($("#mensagemErroCampoVazio_input").length) {
+		$("#mensagemErroCampoVazio_input").focus();
+		focarMensagemErro = false;
+	}
+	}
+});
+
