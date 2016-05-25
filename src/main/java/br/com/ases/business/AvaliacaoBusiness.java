e4e645ce7802faa29aac583441f96a75a7231927
gibran <gibransodre@gmail.com>
2016-05-25 14:02:13 -0300

    Adicionados novos métodos para o depara com o eselo

--- src/main/java/br/com/ases/business/AvaliacaoBusiness.java
+++ src/main/java/br/com/ases/business/AvaliacaoBusiness.java
@@ -2,9 +2,11 @@ package br.com.ases.business;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.servlet.ServletContext;
 
+import net.htmlparser.jericho.Source;
 import br.com.ases.controller.EseloController.Nota;
 import br.com.ases.domain.OccurrenceKey;
 import br.com.checker.emag.Occurrence;
@@ -13,8 +15,11 @@ import br.com.checker.emag.SummarizedOccurrence;
 
 
 public interface AvaliacaoBusiness {
-	Nota obterNota(List<SummarizedOccurrence> occurrences,String url);
+	Nota obterNota(List<SummarizedOccurrence> occurrences,String url, List<Entry<OccurrenceKey,List<Occurrence>>> mapaListaOcorrencias);
+	Nota obterNotaEselo(Source html, String url, List<Entry<OccurrenceKey,List<Occurrence>>> mapaListaOcorrencias);
 	Map<OccurrenceKey,Map<String,List<Occurrence>>> retornarCriterios(Map<OccurrenceClassification,List<Occurrence>> resultadoAvaliacao);
+	List<Entry<OccurrenceKey,List<Occurrence>>> retornarCriteriosTeste(Map<OccurrenceClassification,List<Occurrence>> resultadoAvaliacao);
 	void initEseloProperties(ServletContext servletContext);
+	void initDeparaAsesComEseloProperties(ServletContext servletContext);
 	public int[] getErrorCount(boolean isCss,String url);
 }
