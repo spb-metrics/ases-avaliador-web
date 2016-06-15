610f42174886eefcf4a07ef33198a185ea503dba
gibran <gibransodre@gmail.com>
2016-06-15 09:19:36 -0300

    Substituição do agente de conexão (jakarta).

--- src/main/java/br/com/ases/infra/WebChecker.java
+++ src/main/java/br/com/ases/infra/WebChecker.java
@@ -45,7 +45,8 @@ public class WebChecker {
 	
 	public WebChecker withGetRequest() {
 		this.method = new GetMethod(this.url);
-		this.method.setRequestHeader("user-agent", "Mozilla/5.0");
+		//this.method.setRequestHeader("user-agent", "Mozilla/5.0");
+		this.method.setRequestHeader("http.agent", "Jakarta Commons-HttpClient/3.1");
 		this.method.setFollowRedirects(true);
 		return this;
 	}
